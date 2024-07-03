3c88cc5e1f46834e0d1b9ffdf41d54f30fc36b15
aminul@atilimited.net <aminul@atilimited.net>
2024-07-03 16:52:59 +0600

    designation dto

--- src/global-setup/designation/designation.service.ts
+++ src/global-setup/designation/designation.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Designation } from "src/entities/designation.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class DesignationService {}
+export class DesignationService {
+  constructor(
+    @InjectRepository(Designation)
+    public readonly useRepository: Repository<Designation>
+  ) {}
+}
