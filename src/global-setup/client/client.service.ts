caa89d5510e34ea6bc4fbcb8a8e8b05031862602
aminul@atilimited.net <aminul@atilimited.net>
2024-07-06 17:58:50 +0600

    update and delete client by id

--- src/global-setup/client/client.service.ts
+++ src/global-setup/client/client.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Client } from "src/entities/client.entity";
 import { Repository } from "typeorm";
 import { CreateClientDTO } from "./dto/create.client.dto";
+import { UpdateClientDTO } from "./dto/update.client.dto";
 
 @Injectable()
 export class ClientService {
@@ -21,8 +22,8 @@ export class ClientService {
   async getAllActive() {
     return await this.clientRepository
       .createQueryBuilder("Client")
-      .where({ orgId: 1 })      
-      .where({ activeStatus: true })      
+      .where({ orgId: 1 })
+      .where({ activeStatus: true })
       .getMany();
   }
 
@@ -30,12 +31,37 @@ export class ClientService {
     return await this.clientRepository
       .createQueryBuilder("Client")
       .where({ orgId: 1 })
-      .where({ activeStatus: true })  
+      .where({ activeStatus: true })
       .getMany();
   }
 
   async create(createClientDTO: CreateClientDTO) {
     const clientData = this.clientRepository.create(createClientDTO);
     return await this.clientRepository.save(clientData);
+  }
+
+  async update(id: number, updateClientDTO: UpdateClientDTO) {
+    const ifExist = await this.clientRepository.findOne({ where: { id: id } });
+    if (!ifExist) {
+      throw new HttpException("Data not found", HttpStatus.NOT_FOUND);
+    }
+    return await this.clientRepository.update(id, updateClientDTO);
+  }
+
+  async deleteClientById(id: number) {
+    try {
+      const result = await this.clientRepository.delete(id);
+      if (result.affected === 0) {
+        throw new HttpException("Data not found", HttpStatus.NOT_FOUND);
+      }
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        "Failed to delete data",
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
