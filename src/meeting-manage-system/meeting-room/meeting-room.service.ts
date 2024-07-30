fa6f509b2187a7005e588b6135236ee5ce112093
aminul@atilimited.net <aminul@atilimited.net>
2024-07-30 17:47:55 +0600

    get all list

--- src/meeting-manage-system/meeting-room/meeting-room.service.ts
+++ src/meeting-manage-system/meeting-room/meeting-room.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { MeetingRoom } from "src/entities/meetingRoom.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class MeetingRoomService {}
+export class MeetingRoomService {
+  constructor(
+    @InjectRepository(MeetingRoom)
+    public readonly meetingRoomRepository: Repository<MeetingRoom>
+  ) {}
+
+  async getAll() {
+    return await this.meetingRoomRepository.find({
+      order: {
+        id: "DESC",
+      },
+    });
+  }
+
+  async getAllActive() {
+    return await this.meetingRoomRepository.find({
+      where: {
+        activeStatus: true,
+      },
+      order: {
+        id: "DESC",
+      },
+    });
+  }
+
+  async getAllInActive() {
+    return await this.meetingRoomRepository.find({
+      where: {
+        activeStatus: true,
+      },
+      order: {
+        id: "DESC",
+      },
+    });
+  }
+  async getAllActiveByOrganizationWise() {
+    return await this.meetingRoomRepository.find({
+      where: {
+        activeStatus: true,
+        orgId: 1,
+      },
+      order: {
+        id: "DESC",
+      },
+    });
+  }
+}
