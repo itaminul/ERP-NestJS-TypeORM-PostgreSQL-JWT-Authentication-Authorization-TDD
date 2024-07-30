3b8409bf08bbe2fddd930949d645a05f11eae936
aminul@atilimited.net <aminul@atilimited.net>
2024-07-30 18:10:07 +0600

    create meeting roomr

--- src/meeting-manage-system/meeting-room/meeting-room.controller.ts
+++ src/meeting-manage-system/meeting-room/meeting-room.controller.ts
@@ -1,31 +1,104 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  UseFilters,
+  UseGuards,
+} from "@nestjs/common";
 import { MeetingRoomService } from "./meeting-room.service";
 import { GetUserInfo } from "src/users/user.decorator";
 import { userInfo } from "os";
 import { User } from "src/entities/user.entity";
+import { CreateMeetingRoomDTO } from "./dto/create.meeting.room.dto";
+import { AllExceptionsFilter } from "src/exceptionFilter/http-exception.filter";
+import { AuthGuard } from "@nestjs/passport";
+import { RolesGuard } from "src/roles/roles.guard";
+import { Roles } from "src/roles/roles.decorator";
+import { Role } from "src/roles/role.enum";
 
 @Controller("meeting-room")
+@UseFilters(AllExceptionsFilter)
+@UseGuards(AuthGuard("jwt"), RolesGuard)
 export class MeetingRoomController {
   constructor(public readonly meetingRoomService: MeetingRoomService) {}
+  @Get()
+  @Roles(Role.Admin, Role.User)
   async getAll() {
     try {
       const results = await this.meetingRoomService.getAll();
-    } catch (error) {}
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
+  @Get()
+  @Roles(Role.Admin, Role.User)
   async getAllActive() {
     try {
       const results = await this.meetingRoomService.getAllActive();
-    } catch (error) {}
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
+  @Get()
+  @Roles(Role.Admin, Role.User)
   async getAllInactive() {
     try {
       const results = await this.meetingRoomService.getAllInActive();
-    } catch (error) {}
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
+  @Get()
+  @Roles(Role.Admin, Role.User)
   async getAllByOrganizationIdWIse(@GetUserInfo() userInfo: User) {
     try {
       const results =
         await this.meetingRoomService.getAllActiveByOrganizationWise(userInfo);
-    } catch (error) {}
+        return {
+            success: true,
+            status: HttpStatus.OK,
+            results,
+          };
+        } catch (error) {
+          throw error;
+        }
+  }
+
+  @Post()
+  @Roles(Role.Admin, Role.User)
+  async create(
+    @GetUserInfo() userInfo: User,
+    @Body() createMeetingRoomDto: CreateMeetingRoomDTO
+  ) {
+    try {
+      const results = await this.meetingRoomService.create(
+        userInfo,
+        createMeetingRoomDto
+      );
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- src/meeting-manage-system/meeting-room/meeting-room.module.ts
+++ src/meeting-manage-system/meeting-room/meeting-room.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { MeetingRoomController } from './meeting-room.controller';
-import { MeetingRoomService } from './meeting-room.service';
+import { Module } from "@nestjs/common";
+import { MeetingRoomController } from "./meeting-room.controller";
+import { MeetingRoomService } from "./meeting-room.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MeetingRoom } from "src/entities/meetingRoom.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MeetingRoom])],
   controllers: [MeetingRoomController],
-  providers: [MeetingRoomService]
+  providers: [MeetingRoomService],
 })
 export class MeetingRoomModule {}
